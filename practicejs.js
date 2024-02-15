83d8df3a3c981172393c29520120ddd8fe8feb72
Vishal <kumar9835vishal@gmail.com>
2024-02-15 21:23:13 +0530

    first commit

---
+++ practicejs.js
@@ -0,0 +1,12 @@
+function checkvotting_criteri(age){
+    if(age>=18){
+        console.log("you can vote")
+        // alert("you are minor now")
+    }else{
+        console.log("you can vote")
+        // alert("you are minor now")
+    }
+}
+
+
+const checkingAge=checkvotting_criteri(19);
